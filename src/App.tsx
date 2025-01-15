20b6a9b238b29cde0c6b713f2ee411abff85c70c
box-7 <hayainada1981@yahoo.co.jp>
2025-01-15 21:29:03 +0900

    charaui導入、ボタン色変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import { Button } from "@/components/ui/button"
+import { HStack } from "@chakra-ui/react";
+import { Button, Stack, Theme } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
- <h1>テスト</h1>
-    </>
-  )
+    <Stack align="flex-start">
+      <Button variant="surface" colorPalette="teal">
+        Auto Button
+      </Button>
+      <Theme p="4" appearance="dark" colorPalette="teal">
+        <Button variant="surface">Dark Button</Button>
+      </Theme>
+      <Theme p="4" appearance="light" colorPalette="teal">
+        <Button variant="surface">Light Button</Button>
+      </Theme>
+    </Stack>
+  );
 }
 
-export default App
+export default App;
