591edb6a5ba95edb72fb63dc6e1073d13bd1c5e9
box-7 <hayainada1981@yahoo.co.jp>
2025-02-02 08:08:41 +0900

    必須項目にバリデーションエラーを設定

--- src/pages/cards/Register.tsx
+++ src/pages/cards/Register.tsx
@@ -19,24 +19,52 @@ import {
 
 import { Fieldset } from "@chakra-ui/react";
 import { Field } from "@/components/ui/field";
+import { useForm } from "react-hook-form";
+
 
 interface Skill {
         id: number;
         name: string;
 }
 
+interface FormData {
+        id: string; // id プロパティを追加
+        name: string;
+        description: string;
+        github_id: string;
+        qiita_id: string;
+        x_id: string;
+        favoriteSkill: string;
+}
+
+
 const Register: React.FC = () => {
         // Skillを型定義
         const [skills, setSkills] = useState<Skill[]>([]);
-        const [formData, setFormData] = useState({
-                id: "",
-                name: '',
-                description: '',
-                github_id: '',
-                qiita_id: '',
-                x_id: '',
-                favoriteSkill: '',
+        // const [formData, setFormData] = useState({
+        //         id: "",
+        //         name: '',
+        //         description: '',
+        //         github_id: '',
+        //         qiita_id: '',
+        //         x_id: '',
+        //         favoriteSkill: '',
+        // });
+
+
+        const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+                defaultValues: {
+                        id: '',
+                        name: '',
+                        description: '',
+                        github_id: '',
+                        qiita_id: '',
+                        x_id: '',
+                        favoriteSkill: ''
+                }
         });
+
+
 
         // console.log("skills", skills);
         useEffect(() => {
@@ -52,70 +80,119 @@ const Register: React.FC = () => {
                 fetchSkills();
         }, []);
 
-        const handleChange = (
-                e: React.ChangeEvent<
-                        HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-                >
-        ) => {
-                const { name, value } = e.target;
-                setFormData({ ...formData, [name]: value });
-        };
-
-        const handleSubmit = (e: React.FormEvent) => {
-                e.preventDefault();
+        const onSubmit = async (data: FormData) => {
                 // 登録処理を実装
-                console.log("User registered:", formData);
+                console.log("User registered:", data);
         };
 
         return (
-                <form onSubmit={handleSubmit}>
-                        <Fieldset.Root size="lg" maxW="md" onSubmit={handleSubmit}>
+                // <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                        {/* <Fieldset.Root size="lg" maxW="md" onSubmit={handleSubmit}> onSubmit={handleSubmit}> は不要のはず */}
+                        <Fieldset.Root size="lg" maxW="md"  invalid>
                                 <Stack>
                                         <Fieldset.Legend>名刺新規登録</Fieldset.Legend>
                                 </Stack>
 
                                 <Fieldset.Content>
                                         <Field label="好きな英単語 *">
-
-                                                <Input type="text" name="id" value={formData.id} onChange={handleChange} />
+                                                {/* type="text"
+                                                        入力フィールドのタイプをテキスト入力に設定
+                                                        ユーザーがテキストを入力できるフィールドを提供
+                                                name="id"
+                                                        入力フィールドの名前属性を設定
+                                                        フォームデータを送信するときに、この名前属性がキーとして使用される
+                                                        フォームの状態管理やバリデーションの際に、フィールドを識別するために使用
+                                                value={formData.id}
+                                                        入力フィールドの現在の値を設定
+                                                        フォームの状態を反映し、入力フィールドの値を制御
+                                                        formData.id は、コンポーネントの状態（formData）から取得
+                                                onChange={handleChange}
+                                                        入力フィールドの値が変更されたときに呼び出されるイベントハンドラを設定
+                                                        ユーザーが入力フィールドに文字を入力するたびに、この関数が呼び出され、入力値が更新される
+                                                        通常、handleChange 関数は入力フィールドの値を状態（formData）に反映させるために使用 */}
+                                                {/* <Input type="text" name="id" value={ormData.id} onChange={handleChange} /> */}
+                                                <Input
+                                                        type="text"
+                                                        {...register('id', {
+                                                                required: 'IDは必須入力です。',
+                                                                pattern: {
+                                                                        value: /^[A-Za-z]+$/,
+                                                                        message: 'IDは英語文字列のみです。'
+                                                                }
+                                                        })}
+                                                />
+                                                {errors.id && <p>{errors.id.message}</p>}
                                         </Field>
 
                                         <Field label="お名前 *">
-                                                <Input type="text" name="name" value={formData.name} onChange={handleChange} />
+                                                <Input
+                                                        type="text"
+                                                        {...register('name', {
+                                                                required: 'お名前は必須入力です。',
+                                                                maxLength: {
+                                                                        value: 20,
+                                                                        message: 'お名前は20文字以内にしてください。'
+                                                                }
+                                                        })}
+                                                />
+                                                {errors.name && <p>{errors.name.message}</p>}
                                         </Field>
 
                                         <Field label="自己紹介 *">
-                                                <Textarea name="description" value={formData.description} onChange={handleChange}
-                                                        width="100%" // 幅を100%に設定
-                                                        height="100px" // 高さを200pxに設定
+                                                <Textarea
+                                                        {...register('description', {
+                                                                required: '自己紹介は必須入力です。',
+                                                                maxLength: {
+                                                                        value: 50,
+                                                                        message: '自己紹介は50文字以内にしてください。'
+                                                                }
+                                                        })}
+                                                        width="100%"
+                                                        height="100px"
                                                 />
+                                                {errors.description && <p>{errors.description.message}</p>}
                                         </Field>
 
                                         <Field label="好きな技術 *">
                                                 <NativeSelectRoot>
                                                         {/* value プロパティを設定して、選択された値が formData.favoriteSkill にバインドされるようにする */}
-                                                        <NativeSelectField name="favoriteSkill" value={formData.favoriteSkill} onChange={handleChange} placeholder="選択してください">
+                                                        <NativeSelectField
+                                                                {...register('favoriteSkill', {
+                                                                        required: '好きな技術は必須入力です。',
+                                                                })}
+                                                                placeholder="選択してください"
+                                                        >
                                                                 {skills.map((skill) => (
-                                                                        //  各 <option> 要素に key プロパティと value プロパティを設定することにより、選択肢の値が正しく設定される
                                                                         <option key={skill.id} value={skill.id}>
                                                                                 {skill.name}
                                                                         </option>
                                                                 ))}
                                                         </NativeSelectField>
                                                 </NativeSelectRoot>
+                                                {errors.favoriteSkill && <p>{errors.favoriteSkill.message}</p>}
                                         </Field>
                                 </Fieldset.Content>
 
                                 <Field label="GitHub ID">
-                                        <Input type="text" name="github_id" value={formData.github_id} onChange={handleChange} />
+                                        <Input
+                                                type="text"
+                                                {...register('github_id')}
+                                        />
                                 </Field>
 
                                 <Field label="Qiita ID">
-                                        <Input type="text" name="qiita_id" value={formData.qiita_id} onChange={handleChange} />
+                                        <Input
+                                                type="text"
+                                                {...register('qiita_id')}
+                                        />
                                 </Field>
 
                                 <Field label="X ID">
-                                        <Input type="text" name="x_id" value={formData.x_id} onChange={handleChange} />
+                                        <Input
+                                                type="text"
+                                                {...register('x_id')}
+                                        />
                                 </Field>
 
                                 <Button type="submit" variant="surface" colorPalette="teal" size="sm">
