1c4c057ad467256463c59e2de352857224009470
box-7 <hayainada1981@yahoo.co.jp>
2025-02-17 23:13:09 +0900

    tsファイルを実行すれば作成したusersとuser_skillを削除できるを実装

---
+++ batch/index.ts
@@ -0,0 +1,31 @@
+//supabaseをインポートして
+import supabase from "../supabase";
+
+// usersテーブル、user_skillテーブル、の内容を削除するコマンドを作って
+
+// githubactionsのワークフローで実行する
+
+export const deleteUsers = async (): Promise<void> => {
+  const { error } = await supabase.from("users").delete();
+
+  if (error) {
+    throw new Error(`Error deleting users: ${error.message}`);
+  }
+};
+
+export const deleteUserSkill = async (): Promise<void> => {
+  const { error } = await supabase.from("user_skill").delete();
+
+  if (error) {
+    throw new Error(`Error deleting user_skill: ${error.message}`);
+  }
+};
+
+export const deleteAll = async (): Promise<void> => {
+  await deleteUsers();
+  await deleteUserSkill();
+};
+
+deleteAll().catch((error) => {
+  console.error("Error executing deleteAll:", error);
+});
